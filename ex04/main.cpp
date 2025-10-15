91ebeee94bc14ff43f0fafc2612203b5856cff82
Valentin Martel <vmartel@f4r2s1.clusters.42paris.fr>
2025-10-15 12:14:43 +0200

    push it to the limits

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vmartel <vmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 16:58:44 by vmartel           #+#    #+#             */
-/*   Updated: 2025/10/15 11:40:56 by vmartel          ###   ########.fr       */
+/*   Updated: 2025/10/15 11:46:39 by vmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void printError(std::string msg)
 {
 	std::cerr << msg << std::endl;
-	exit(1);
+	// exit(1);
 }
 
 bool isFileReadable(const std::string& filename)
@@ -42,17 +42,26 @@ std::string replaceAll(std::string content, const std::string& s1, const std::st
 int main(int ac, char **av)
 {
 	if (ac != 4)
+	{
 		printError("Wrong numbers of args..");
+		return (0);
+	}
 	std::string filename = av[1];
 	std::string s1 = av[2];
 	std::string s2 = av[3];
 
 	if (!isFileReadable(av[1]))
+	{
 		printError("Files not readable...");
+		return (0);
+	}
 		
 	std::ifstream input(filename.c_str());
 	if (!input.is_open())
+	{
 		printError("Error opening file...");
+		return (0);
+	}
 	std::string content;
 	std::string line;
 	
@@ -70,7 +79,10 @@ int main(int ac, char **av)
 		return (0);
 	std::ofstream output((filename + ".replace").c_str());
 	if (!output.is_open())
+	{
 		printError("Error creating output file.");
+		return (0);
+	}
 	output << replaced;
 	output.close();
 	return (0);
